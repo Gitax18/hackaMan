cdc528b2f0b374b09ba23adaad041f52b79aea5f
Gitax18 <lusifer0418@gmail.com>
2025-02-28 01:03:21 +0530

    add routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import AllContextsProvider from "./context/AllContextsProvider";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AllContextsProvider>
+      <RouterProvider router={router} />
+    </AllContextsProvider>
+  </StrictMode>
+);
