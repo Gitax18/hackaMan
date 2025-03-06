a2cbd27c622e3efd1184865542bbcadc5cd220b6
Gitax18 <lusifer0418@gmail.com>
2025-03-06 13:44:57 +0530

    add validation for login

--- backend/middlewares/validation.auth.js
+++ backend/middlewares/validation.auth.js
@@ -17,3 +17,18 @@ exports.signupValidation = function (req, res, next) {
 
   next();
 };
+
+exports.loginValidation = function (req, res, next) {
+  const schema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(8).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json(Error("Bad Request", error, false));
+  }
+
+  next();
+};
