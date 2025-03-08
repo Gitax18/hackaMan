5d61d0afe8f589c21de3c088da274567abbc07ac
Gitax18 <lusifer0418@gmail.com>
2025-03-08 21:52:57 +0530

    add route for verifyOTP

--- backend/routes/route.auth.js
+++ backend/routes/route.auth.js
@@ -2,12 +2,19 @@ const express = require("express");
 const {
   signupValidation,
   loginValidation,
+  otpValidation,
 } = require("../middlewares/validation.auth");
-const { postSignup, postLogin } = require("../controllers/controller.auth");
+const {
+  postSignup,
+  postLogin,
+  postVerifyOTP,
+} = require("../controllers/controller.auth");
 
 const router = express.Router();
 
 router.post("/signup", signupValidation, postSignup);
+
+router.post("/verifyotp", otpValidation, postVerifyOTP);
 
 router.post("/login", loginValidation, postLogin);
 
